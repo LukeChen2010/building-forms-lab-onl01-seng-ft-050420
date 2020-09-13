4b97ebcb1257fd51a1d41053dcbb141362be2c99
Luke Chen <luke.chen.2010@gmail.com>
2020-09-13 18:44:52 +0000

    Automatically backed up by Learn

--- src/containers/BandsContainer.js
+++ src/containers/BandsContainer.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react'
 import BandInput from '../components/BandInput'
 import { connect } from 'react-redux'
+<<<<<<< HEAD
 import Bands from '../components/Bands'
+=======
+>>>>>>> 4b1b68c3c74ac6dd2ccefc90356a85d92366c49c
 
 
 class BandsContainer extends Component {
